5713aff24cfbe419fce156a6349db5c96434e93c
dudleyspence <dudleyspence5@gmail.com>
2024-07-09 22:38:26 +0200

    adds completed task 4

--- week_1_katas/04_politeRequests.js
+++ week_1_katas/04_politeRequests.js
@@ -1,5 +1,47 @@
-const politeRequests = () => {};
+const politeRequests = (instruction, namesArray, isPolite) => {
+    editedRequests = []
+
+
+    for (let name of namesArray){
+
+        let request = ''
+
+        if (isPolite) {
+            request = instruction.replace('%', 'please ' + name)
+        } else {
+            request = instruction.replace('%', name)
+        }
+
+        editedRequests.push(request)
+    }
+
+    return editedRequests
+
+};
 
 
 
 module.exports = politeRequests;
+
+
+
+
+/*
+QUESTION 4:
+
+This function takes in three arguments:
+
+A string which is an instruction, with a "%" in place of a person's name. Eg. "Feed the cat %"
+An array of names, e.g. ["Katherine", "Tim", "Caz", "Zanna"]
+A boolean which determines whether or not the returned instructions will be polite, with "please" added before their name.
+It must return an array of instruction strings, with the % replaced with each of the names in the array in turn. If the boolean is true, each instruction must also have "please" added to the string before their name.
+
+For example:
+
+politeRequests("Go to the bank %", ["Katherine", "Tim"], true)
+// returns ["Go to the bank please Katherine", "Go to the bank please Tim"]
+
+politeRequests("Fetch me a drink %", ["Caz", "Zanna"], false)
+// returns ["Fetch me a drink Caz", "Fetch me a drink Zanna"]
+
+*/
--- week_1_katas/__tests__/04_politeRequests.test.js
+++ week_1_katas/__tests__/04_politeRequests.test.js
@@ -1,7 +1,60 @@
 const politeRequests = require("../04_politeRequests");
 
 describe("politeRequests", () => {
-  it("", () => {
-    // first test here
+  it("Output is of type array", () => {
+
+    expected = true
+    actual = Array.isArray(politeRequests("Go to the bank %", ["Katherine", "Tim"], true))
+
+    expect(actual).toBe(expected)
+
   });
+
+  it("Output array contains a string for each of the names", () => {
+
+    expected = 2
+    actual = (politeRequests("Go to the bank %", ["Katherine", "Tim"], true)).length
+
+    expect(actual).toBe(expected)
+
+  });
+
+  it("Output array contains a correct string when given one name and isPolite=false", () => {
+
+    expected = ["Fetch me a drink Caz"]
+    actual = politeRequests("Fetch me a drink %", ["Caz"], false)
+
+    expect(actual).toEqual(expected)
+
+  });
+
+  it("Output array contains a correct string when given one name and isPolite=true", () => {
+
+    expected = ["Fetch me a drink please Caz"]
+    actual = politeRequests("Fetch me a drink %", ["Caz"], true)
+
+    expect(actual).toEqual(expected)
+
+  });
+
+  it("Output array contains a correct strings when given multiple name and isPolite=true", () => {
+
+    expected = ["Go to the bank please Katherine", "Go to the bank please Tim"]
+    actual = politeRequests("Go to the bank %", ["Katherine", "Tim"], true)
+
+    expect(actual).toEqual(expected)
+
+  });
+
+  it("Output array contains a correct strings when given multiple name and isPolite=false", () => {
+
+    expected = ["Fetch me a drink Caz", "Fetch me a drink Zanna"]
+    actual = politeRequests("Fetch me a drink %", ["Caz", "Zanna"], false)
+
+    expect(actual).toEqual(expected)
+
+  });
+
+
+
 });
