9b7090fe318a3497e42ad9c3226568085d99e6b0
dudleyspence <dudleyspence5@gmail.com>
2024-07-10 00:24:06 +0200

    completed Kata

--- week_1_katas/05_splitTheData.js
+++ week_1_katas/05_splitTheData.js
@@ -1,3 +1,79 @@
-const splitTheData = () => {};
+const splitTheData = (gamerDataArray) => {
+
+    gamersObject = {people: [], games: {}, gamesPeopleLike: []}
+
+    gamerDataArray.forEach((gamer) => {
+        gamersObject.people.push({id: gamer.id, name: gamer.name})
+        gamer.faveGames.forEach((game) => {
+            const allGames = Object.values(gamersObject.games)
+            if (!allGames.includes(game)){
+                gamersObject.games[allGames.length+1] = game
+            }
+
+            const game_id = Object.keys(gamersObject.games).find(key => gamersObject.games[key] === game)
+
+            gamersObject.gamesPeopleLike.push([gamer.id, Number(game_id)])
+             
+        })
+        
+        
+    });
+
+    return gamersObject
+
+};
 
 module.exports = splitTheData;
+
+
+
+/*
+
+
+QUESTION 5:
+
+This function takes in data in the following format about people and their favourite games.
+
+const gamerData = [
+  {
+    id: 1,
+    name: "Katherine",
+    faveGames: ["Minecraft", "Factorio"],
+  },
+  {
+    id: 2,
+    name: "Tim",
+    faveGames: ["Bingo", "Minecraft"],
+  },
+];
+The function splitTheData returns the data reorganised into a new object with keys of people, games and gamesPeopleLike.
+
+So splitTheData(gamerData) returns:
+
+{
+    people: [
+        {
+            id: 1
+            name: 'Katherine'
+        },
+        {
+            id: 2
+            name: 'Tim'
+        }
+    ],
+    games: {
+        1: 'Minecraft',
+        2: 'Factorio',
+        3: 'Bingo'
+    }
+    gamesPeopleLike: [
+      [1,1], // Katherine likes Minecraft
+      [1,2], // Katherine likes Factorio
+      [2,3], // Tim likes Bingo
+      [2,1]  // Tim likes Minecraft
+    ]
+}
+It's important we don't change the original data, this function will need to be pure.
+
+Build up your tests gradually and flexibly, adding separate tests for each key value pair in the returned object.
+*/
--- week_1_katas/__tests__/05_splitTheData.test.js
+++ week_1_katas/__tests__/05_splitTheData.test.js
@@ -1,7 +1,215 @@
 const splitTheData = require("../05_splitTheData");
 
 describe("splitTheData", () => {
-  it("", () => {
-    // first test here
+  it("function returns an object", () => {
+    const expected = 'object'
+    const actual = typeof (splitTheData([{
+      id: 1,
+      name: "Katherine",
+      faveGames: ["Minecraft", "Factorio"],
+    }]))
+
+    expect(actual).toEqual(expected)
+
+
   });
+
+
+  it("Input is not mutated", () => {
+    const input = [
+      {
+        id: 1,
+        name: "Katherine",
+        faveGames: ["Minecraft", "Factorio"],
+      }]
+
+      const input2 = [
+        {
+          id: 1,
+          name: "Katherine",
+          faveGames: ["Minecraft", "Factorio"],
+        }]
+
+    splitTheData(input)
+
+    expect(input).toEqual(input2)
+
+  });
+
+
+  it("Input is not the same as the output", () => {
+
+    const input = [
+      {
+        id: 1,
+        name: "Katherine",
+        faveGames: ["Minecraft", "Factorio"],
+      }]
+
+    const actual = splitTheData(input)
+
+    expect(input).not.toBe(actual)
+
+  });
+
+  it("Output object has correct values for the people property", () => {
+
+    const input = [
+      {
+        id: 1,
+        name: "Katherine",
+        faveGames: ["Minecraft", "Factorio"],
+      },
+      {
+        id: 2,
+        name: "Tim",
+        faveGames: ["Bingo", "Minecraft"],
+      },
+    ];
+
+    const actual = splitTheData(input).people
+    const expected = [
+      {
+          id: 1,
+          name: 'Katherine'
+      },
+      {
+          id: 2,
+          name: 'Tim'
+      }]
+
+    expect(actual).toEqual(expected)
+
+  });
+
+  it("Output object has correct values for the games property", () => {
+
+    const input = [
+      {
+        id: 1,
+        name: "Katherine",
+        faveGames: ["Minecraft", "Factorio"],
+      },
+      {
+        id: 2,
+        name: "Tim",
+        faveGames: ["Bingo", "Minecraft"],
+      },
+    ];
+
+    const actual = splitTheData(input).games
+    const expected = {
+      1: 'Minecraft',
+      2: 'Factorio',
+      3: 'Bingo'
+  }
+    expect(actual).toEqual(expected)
+
+  });
+
+
+  it("Output object has correct values for the gamesPeopleLike property", () => {
+
+    const input = [
+      {
+        id: 1,
+        name: "Katherine",
+        faveGames: ["Minecraft", "Factorio"],
+      },
+      {
+        id: 2,
+        name: "Tim",
+        faveGames: ["Bingo", "Minecraft"],
+      },
+    ];
+
+    const actual = splitTheData(input).gamesPeopleLike
+    const expected = [
+      [1,1], 
+      [1,2], 
+      [2,3], 
+      [2,1]  
+    ]
+
+    expect(actual).toEqual(expected)
+
+  });
+
+  it("Output object has correct values for the games property", () => {
+
+    const input = [
+      {
+        id: 1,
+        name: "Katherine",
+        faveGames: ["Minecraft", "Factorio"],
+      },
+      {
+        id: 2,
+        name: "Tim",
+        faveGames: ["Bingo", "Minecraft"],
+      },
+    ];
+
+    const actual = splitTheData(input).games
+    const expected = {
+      1: 'Minecraft',
+      2: 'Factorio',
+      3: 'Bingo'
+  }
+    expect(actual).toEqual(expected)
+
+  });
+
+
+  it("Output object has correct values for all properties", () => {
+
+    const input = [
+      {
+        id: 1,
+        name: "Katherine",
+        faveGames: ["Minecraft", "Factorio"],
+      },
+      {
+        id: 2,
+        name: "Tim",
+        faveGames: ["Bingo", "Minecraft"],
+      },
+    ];
+
+    const actual = splitTheData(input)
+    const expected = {
+      people: [
+          {
+              id: 1,
+              name: 'Katherine'
+          },
+          {
+              id: 2,
+              name: 'Tim'
+          }
+      ],
+      games: {
+          1: 'Minecraft',
+          2: 'Factorio',
+          3: 'Bingo'
+      },
+      gamesPeopleLike: [
+        [1,1], 
+        [1,2],
+        [2,3], 
+        [2,1]  
+      ]
+  }
+
+    expect(actual).toEqual(expected)
+
+  });
+
+
+
 });
+
+
+
+
+
